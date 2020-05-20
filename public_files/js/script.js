7ed43a6fdb70b6768d73265d9e3c59d55fb05398
Shash833 <58815647+Shash833@users.noreply.github.com>
2020-05-20 23:08:19 +1000

    front-end and third party API functionality

---
+++ public_files/js/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    //event listener for city search button on homepage
+    $(".searchCity").on("click", function (event) {
+        console.log("clicked")
+        const city = $("#inputCity").val().trim()
+        //load search results when button is clicked
+        if (city) { location.assign(`restaurants/${city}`) }
+
+    })
+
+    //event listener for search result selection
+    $(".restaurant").on("click", function (event) {
+        let restaurantID = parseInt($(this).data("id"))
+        //load restaurant page when button is clicked
+        location.assign(`../${restaurantID}`)
+    })
+
+    //TODO: BELOW TODO'S & NEED TO FINALISE DB TABLES FOR MENU ITEMS
+    //confirm order button 
+    $(".confirm_order").on("click", function (event) {
+        //TODO: retrieve order id
+
+        //TODO: select menu selections (for menu selections, retrieve restaurant ID, item ID and quantity)
+        location.assign(`order/placeholder`)
+        //TODO: replace placeholder with order ID
+    })
+})
