925aa118ca1b54a2f7a9dc47e5f963bc35c49252
Atul Mahajan <luta_atul@hotmail.com>
2020-05-20 23:54:52 +1000

    Adding registration changes.

---
+++ config/orm.js
@@ -0,0 +1,75 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+// Object for all our SQL statement functions.
+var orm = {
+  //   selectAll: function (tableInput, cb) {
+  //     var queryString = "SELECT * FROM " + tableInput + ";";
+  //     connection.query(queryString, function (err, result) {
+  //       if (err) {
+  //         throw err;
+  //       }
+  //       cb(result);
+  //     });
+  //   },
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  //   updateOne: function (table, objColVals, condition, cb) {
+  //     var queryString = "UPDATE " + table;
+
+  //     queryString += " SET ";
+  //     queryString += objToSql(objColVals);
+  //     queryString += " WHERE ";
+  //     queryString += condition;
+
+  //     connection.query(queryString, function (err, result) {
+  //       if (err) {
+  //         throw err;
+  //       }
+  //       cb(result);
+  //     });
+  //   },
+};
+
+// Exporting the orm object for the model (burger.js).
+module.exports = orm;
